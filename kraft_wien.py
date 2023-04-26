2f2a81bcc67821149b79ed399e67d2d59159ca59
anatexis <anatexis@users.noreply.github.com>
2023-04-26 15:26:03 +0200

    new structure

--- kraft_wien.py
+++ kraft_wien.py
@@ -7,32 +7,8 @@ from io import StringIO
 import pydeck as pdk
 from shapely.geometry import shape, Polygon
 
-
-# Download the CSV file
-url = 'https://www.wien.gv.at/gogv/l9ogdviebezbiztecveh2002f'
-@st.cache_data
-def load_data(url):
-    data = pd.read_csv(url, sep=';', header=1, decimal=',', thousands='.')
-    return data
-
-csv_data = load_data(url)
-
-# App title
-st.title('District Data Plotter')
-
-# # Upload CSV file
-# uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
-#
-# if uploaded_file:
-#     csv_data = pd.read_csv(uploaded_file, sep=';', header=1, decimal=',')
-
-# Change DISCTRICT_CODE to 4 numbers:
-
-csv_data = csv_data.assign(DISTRICT = csv_data['DISTRICT_CODE'].astype(str).str.slice(start=1, stop=5).astype(int))
-
-# Dropdown for selecting value
-value_options = list(csv_data.columns[5:-1])
-selected_value = st.selectbox('Select a value to plot:', value_options)
+CSV_URL = 'https://www.wien.gv.at/gogv/l9ogdviebezbiztecveh2002f'
+VIENNA_DISTRICTS_GEOJSON_URL = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json"
 
 # Map short codes to long titles
 title_mapping = {
@@ -50,24 +26,29 @@ title_mapping = {
     'BIK_DENSITY': 'Zugelassene Kraftfahrzeuge - Krafträder pro 1.000 EinwohnerInnen'
 }
 
-# Display the long title according to the selected_value
-st.header(title_mapping[selected_value][:26])
-st.subheader(title_mapping[selected_value][28:])
-# get the data for whole Vienna (DISTRICT_CODE: 9000)
-st.text('Ganz Wien: ' + str(csv_data.loc[csv_data['DISTRICT_CODE'] == 90000, selected_value].values[0]) + title_mapping[selected_value][28:])
+@st.cache_data
+def load_data(url):
+    """Load data from a CSV file."""
+    data = pd.read_csv(url, sep=';', header=1, decimal=',', thousands='.')
+    return data
 
-# Load district boundaries GeoJSON for Vienna
-vienna_districts_geojson_url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json"
-
-# Merge data with GeoJSON
-vienna_districts_geojson = requests.get(vienna_districts_geojson_url).json()
 
 @st.cache_data
 def merge_data_with_geojson(district_data, geojson, value_key):
+    """Merge district data with GeoJSON data."""
     for feature in geojson["features"]:
         district = feature["properties"].get("BEZNR")
         if district:
-            feature["properties"][value_key] = float(district_data.loc[district, value_key] if district in district_data.index else 0)
+            # Find the row that matches the district value
+            matching_row = district_data[district_data['DISTRICT'] == district]
+
+            # If there's a matching row, set the value_key, otherwise set it to 0
+            if not matching_row.empty:
+                feature["properties"][value_key] = float(
+                    matching_row[value_key].values[0])
+            else:
+                feature["properties"][value_key] = 0.0
+
             geom = shape(feature["geometry"])
             if geom.is_valid:
                 centroid = geom.centroid
@@ -76,8 +57,10 @@ def merge_data_with_geojson(district_data, geojson, value_key):
                 feature["properties"]["centroid"] = (0, 0)
     return geojson
 
+
 @st.cache_data
 def generate_text_data(geojson, value_key):
+    """Generate text data for district values."""
     text_data = []
 
     for feature in geojson["features"]:
@@ -93,12 +76,9 @@ def generate_text_data(geojson, value_key):
 
     return text_data
 
-
-merged_data = merge_data_with_geojson(csv_data, vienna_districts_geojson, selected_value)
-
 @st.cache_data
-def create_2d_map_pydeck(geojson, value_key):
-    # Create the GeoJSON layer
+def create_2d_map_pydeck(geojson, value_key, year):
+    """Create a 2D map using pydeck."""
     geojson_layer = pdk.Layer(
         "GeoJsonLayer",
         geojson,
@@ -108,6 +88,7 @@ def create_2d_map_pydeck(geojson, value_key):
         get_line_width=10,
         pickable=True,
         auto_highlight=True
+
     )
 
     # Generate the data for the Text layer
@@ -121,19 +102,15 @@ def create_2d_map_pydeck(geojson, value_key):
         get_position="position",
         get_text="text",
         get_size=13,
-        get_color=[0,40,0],
+        get_color=[0, 40, 0],
         get_angle=0,
         get_text_anchor='"middle"',
         get_alignment_baseline='"center"',
         auto_highlight=True
     )
 
-    # Set the initial view state
-    #view_state = pdk.ViewState(latitude=48.1351, longitude=11.5820, zoom=10)
-
-    # Render the map
     return pdk.Deck(
-        layers=[geojson_layer, text_layer],
+               layers=[geojson_layer, text_layer],
         initial_view_state={
             "latitude": 48.2082,
             "longitude": 16.3738,
@@ -141,13 +118,64 @@ def create_2d_map_pydeck(geojson, value_key):
             "pitch": 0,
             "bearing": 0,
         },
+        tooltip={
+            "html": "<b>Elevation Value:</b> {'NAMEK_NUM'}",
+            "style": {
+                "backgroundColor": "steelblue",
+                "color": "white"
+            }
+        },
         map_provider="mapbox", map_style=None)
 
+def main():
+    st.title('Kraftfahrzeugbestand in Wien')
+    
+    csv_data = load_data(CSV_URL)
+    csv_data = csv_data.assign(DISTRICT=csv_data['DISTRICT_CODE'].astype(str).str.slice(start=1, stop=3).astype(int))
+    
+    # Dropdown for selecting value
+    value_options = list(csv_data.columns[5:-1])
+    selected_value = st.selectbox('Bitte Variable auswählen: ', value_options)
+    
+    # Add this after the 'Select a value to plot' dropdown
+    year_min = int(csv_data['REF_YEAR'].min())
+    year_max = int(csv_data['REF_YEAR'].max())
+    selected_year = st.slider('Select a year:', year_min, year_max, year_max)
+    
+    # Filter data for selected year
+    csv_data_filtered = csv_data.loc[csv_data['REF_YEAR'] == selected_year]
+    
+    # Display the long title according to the selected_value
+    st.header(title_mapping[selected_value][:26])
+    st.subheader(title_mapping[selected_value][28:])
+    
+    # Load district boundaries GeoJSON for Vienna
+    vienna_districts_geojson = requests.get(VIENNA_DISTRICTS_GEOJSON_URL).json()
+    
+    # Merge data with GeoJSON
+    merged_data = merge_data_with_geojson(csv_data_filtered, vienna_districts_geojson, selected_value)
+    
+    # Create the 2D map with numbers on districts using pydeck
+    map_2d_pydeck = create_2d_map_pydeck(merged_data, selected_value, selected_year)
+    
+    # Display the map in Streamlit
+    st.pydeck_chart(map_2d_pydeck)
+    
+    st.subheader(f'Daten für das Jahr {selected_year}')
+    st.dataframe(csv_data_filtered[csv_data_filtered.columns[5:-1]])
+    
+    st.markdown('''
+
+    Datenquelle und Datensätze:
+
+    Stadt Wien - https://data.wien.gv.at
+
+    [Zugelassene Kraftfahrzeuge seit 2002 - Bezirke Wien](https://www.data.gv.at/katalog/de/dataset/vie-bez-biz-tec-veh-2002f)
+     & [Bezirksgrenzen Wien](https://www.data.gv.at/katalog/de/dataset/stadt-wien_bezirksgrenzenwien)
+    
+                ''')
 
 
+if __name__ == "__main__":
+    main()
 
- # Create the 2D map with numbers on districts using pydeck
-map_2d_pydeck = create_2d_map_pydeck(merged_data, selected_value)
-
-# Display the map in Streamlit
-st.pydeck_chart(map_2d_pydeck)
